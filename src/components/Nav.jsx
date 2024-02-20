59df00ca466f49f9f6fc22a32b1eb3e489d3854e
teerthraj_verma <teerthrajverma181001@gmail.com>
2024-02-20 12:41:13 +0530

    hey

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   const container = useRef();
@@ -20,50 +21,67 @@ const Nav = () => {
     gsap.to(`.${className} .overlay`, { xPercent: -100 });
   };
 
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    // Get the target URL from the NavLink
+    const targetUrl = "/pages";
+
+    // Update the URL and reload the page
+    window.location.reload();
+    window.location.href = targetUrl;
+  };
   return (
     <div ref={container} className="flex items-center justify-center h-full ">
       <div className="flex items-center justify-center w-full ">
         <div>
           <div className="overflow-hidden tracking-tighter text-8xl">
-            <a
+            <NavLink
               // href="/about-us"
-              href="/pages"
+              // href="/pages"
+              to={"/pages"}
+              onClick={handleClick}
               className="relative about"
               onMouseEnter={() => onMouseEnter("about")}
               onMouseLeave={() => onMouseLeave("about")}
             >
               <span className="z-10 px-4 text">About.</span>
               <div className="bg-[#fff500] absolute w-[35rem]  h-full top-0 left-0 -z-10 overlay"></div>
-            </a>
+            </NavLink>
           </div>
           <div className="overflow-hidden tracking-tighter text-8xl">
-            <a
+            <NavLink
               // href="/"
-              href="/pages"
+              to={"/pages"}
+              onClick={handleClick}
               className="relative contact"
               onMouseEnter={() => onMouseEnter("contact")}
               onMouseLeave={() => onMouseLeave("contact")}
             >
               <span className="z-10 px-4 text">Contact.</span>
               <div className="bg-[#fff500] absolute w-[35rem]  h-full top-0 left-0 -z-10 overlay"></div>
-            </a>
+            </NavLink>
           </div>
           <div className="overflow-hidden tracking-tighter text-8xl">
-            <a
+            <NavLink
               // href="/thoughts"
-              href="/pages"
+              // href="/pages"
+              to={"/pages"}
+              onClick={handleClick}
               className="relative print "
               onMouseEnter={() => onMouseEnter("print")}
               onMouseLeave={() => onMouseLeave("print")}
             >
               <span className="z-10 px-4 text">Thought.</span>
               <div className="bg-[#fff500] absolute w-[35rem]  h-full top-0 left-0 -z-10 overlay"></div>
-            </a>
+            </NavLink>
           </div>
           <div className="overflow-hidden tracking-tighter text-8xl">
             <a
               // href="/portfolio"
-              href="/pages"
+              // href="/pages"
+              to={"/pages"}
+              onClick={handleClick}
               className="relative portfolio"
               onMouseEnter={() => onMouseEnter("portfolio")}
               onMouseLeave={() => onMouseLeave("portfolio")}
@@ -75,7 +93,9 @@ const Nav = () => {
           <div className="overflow-hidden tracking-tighter text-8xl">
             <a
               // href="/news"
-              href="/pages"
+              // href="/pages"
+              to={"/pages"}
+              onClick={handleClick}
               className="relative news"
               onMouseEnter={() => onMouseEnter("news")}
               onMouseLeave={() => onMouseLeave("news")}
